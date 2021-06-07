179f3867e7503b90ceb6a5bd3a4e413e64b378a4
Sebastian Scheib <stackpointerex@gmail.com>
2021-06-07 18:00:47 -0300

    Inicios de automation

--- src/test/java/test/SearchTest.java
+++ src/test/java/test/SearchTest.java
@@ -1,16 +1,34 @@
 package test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchTest {
 	
 	@Test
-	public void testSearchwithResults() {
+	public void testSearchWithResult() {
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://automationpractice.com/");
+		driver.findElement(By.id("search_query_top")).sendKeys("dress");
+		driver.findElement(By.name("submit_search")).click();
+		String titleText = driver.findElement(By.className("lighter")).getText();
+		Assert.assertTrue(titleText.contains("DRESS"),"Expected to contain DRESS but not found");
+		driver.close();
+	}
+	
+	@Test
+	public void testSearchWithoutResult() {
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://automationpractice.com/");
+		driver.findElement(By.id("search_query_top")).sendKeys("hola mundo");
+		driver.findElement(By.name("submit_search")).click();
+		String errorText = driver.findElement(By.xpath("//*[@id=\'center_column\']/p")).getText();
+		Assert.assertTrue(errorText.contains("No results"),"Expected to contain no results text");
 		driver.close();
 	}
 }
